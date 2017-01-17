8d155daea4edfe49b8356b003020ae2c28641946
Martin Wang <jiahaow@ca.ibm.com>
2017-01-17 11:37:58 -0500

    Remove collab dependency in single user mode

--- modules/orionode/index.js
+++ modules/orionode/index.js
@@ -75,10 +75,14 @@ function startServer(options) {
 				return res.status(200).json({UserName: req.user.username});
 			});
 		} else {
+<<<<<<< HEAD
 			app.use(require(options.configParams["login.module"] || "./lib/user")(options));
+=======
+			app.use(require('./lib/user')(options));
+			app.use('/sharedWorkspace', checkCollabAuthenticated, require('./lib/sharedWorkspace')({ root: '/sharedWorkspace/tree/file', fileRoot: '/file', options: options }));
+>>>>>>> Remove collab dependency in single user mode
 		}
 		
-		app.use('/sharedWorkspace', checkCollabAuthenticated, require('./lib/sharedWorkspace')({ root: '/sharedWorkspace/tree/file', fileRoot: '/file', options: options }));
 		app.use('/site', checkAuthenticated, require('./lib/sites')(options));
 		app.use('/task', checkAuthenticated, require('./lib/tasks').router({ root: contextPath + '/task' }));
 		app.use('/filesearch', checkAuthenticated, require('./lib/search')(options));
