eclipse-archived/orion.client

Debugger implementation
This commit contains both the frontend and the backend.

The frontend contains a debug service, a slideout pane in the side bar
(debug UI) and a deploy service.

The backend talks to debug adapters which implement VSCode Debug
Protocol. it can be used either as a module of Orionode or a standalone
server.

Change-Id: Ib6f17715f498301fa699bfad86a49ca6e667c7c9
Signed-off-by: Martin Wang <jiahaow@ca.ibm.com>

modules/orionode.debug.server/lib/debugServer.js
@@ -1,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+'use strict';
+
+var logger = require('./logger');
+var AdapterManager = require('./adapterManager');
+
+var CONNECTION_TIMEOUT = 10000;
+
+/**
+ * @param {Map.<string, DebugAdapter>} adapterPool
+ * @return {function}
+ */
+module.exports = function createDebugServer(adapterPool) {
+    return function(socket) {
+        var connectionId;
+        do {
+            connectionId = Math.floor(Math.random() * 0x1000000).toString(16);
+        } while (adapterPool.has(connectionId));
+        var adapter = null;
+
+        socket.emit('idle');
+
+        logger.log('Socket ' + connectionId + ' connected.', logger.MessageType.VERBOSE);
+
+        socket.on('disconnect', function() {
+            logger.log('Socket ' + connectionId + ' disconnected.', logger.MessageType.VERBOSE);
+            if (adapter) {
+                adapter.dispose();
+            }
+        });
+
+        socket.on('init', function(request, callback) {
+            try {
+                // Initialize new adapter
+                adapter = AdapterManager.createAdapter(request.arguments.adapterID);
+                adapterPool.set(connectionId, adapter);
+                adapter.on('request', function (request) {
+                    socket.emit('request', request, function(response) {
+                        adapter.sendResponse(response);
+                    });
+                });
+
+                adapter.on('event', function (event) {
+                    socket.emit('event', event);
+                });
+
+                adapter.on('disposed', function() {
+                    adapter = null;
+                    adapterPool.delete(connectionId);
+                    socket.emit('idle');
+                    logger.log(request.arguments.adapterID + ' adapter ended for ' + connectionId + '.', logger.MessageType.VERBOSE);
+                });
+
+                // Send launch command
+                adapter.sendRequest(request.command, request.arguments, CONNECTION_TIMEOUT, function(response) {
+                    if (response.success) {
+                        socket.emit('ready', connectionId);
+                    }
+                    if (callback) {
+                        callback(response);
+                    }
+                });
+
+                logger.log(request.arguments.adapterID + ' adapter started for ' + connectionId + '.', logger.MessageType.VERBOSE);
+            } catch (ex) {
+                socket.emit('fail', ex.message);
+                adapter = null;
+                adapterPool.delete(connectionId);
+            }
+        });
+
+        socket.on('request', function(request, callback) {
+            if (!adapter) {
+                socket.emit('fail', 'Not ready yet.');
+                return;
+            }
+            try {
+                adapter.sendRequest(request.command, request.arguments, CONNECTION_TIMEOUT, function(response) {
+                    if (callback) {
+                        callback(response);
+                    }
+                });
+            } catch (ex) {
+                socket.emit('fail', ex.message);
+            }
+        });
+
+        socket.on('event', function(event) {
+            adapter.sendEvent(event);
+        });
+    };
+};

modules/orionode.debug.server/lib/protocol.js
@@ -1,0 +1,81 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *--------------------------------------------------------------------------------------------*/
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+'use strict';
+
+var VSCodeProtocol = require('vscode-debugadapter/lib/protocol');
+
+var ProtocolServer = function() {
+    VSCodeProtocol.ProtocolServer.apply(this, arguments);
+};
+
+ProtocolServer.prototype = Object.create(VSCodeProtocol.ProtocolServer.prototype);
+
+ProtocolServer.prototype.handleEvent = function (event) {
+};
+
+// Add event handler
+ProtocolServer.prototype._handleData = function (data) {
+    this._rawData = Buffer.concat([this._rawData, data]);
+    while (true) {
+        if (this._contentLength >= 0) {
+            if (this._rawData.length >= this._contentLength) {
+                var message = this._rawData.toString('utf8', 0, this._contentLength);
+                this._rawData = this._rawData.slice(this._contentLength);
+                this._contentLength = -1;
+                if (message.length > 0) {
+                    try {
+                        var msg = JSON.parse(message);
+                        if (msg.type === 'request') {
+                            this.dispatchRequest(msg);
+                        }
+                        else if (msg.type === 'response') {
+                            var response = msg;
+                            var clb = this._pendingRequests.get(response.request_seq);
+                            if (clb) {
+                                this._pendingRequests.delete(response.request_seq);
+                                clb(response);
+                            }
+                        }
+                        else if (msg.type === 'event') {
+                            this.handleEvent(msg);
+                        }
+                    }
+                    catch (e) {
+                        this._emitEvent(new messages_1.Event('error'));
+                    }
+                }
+                continue; // there may be more complete messages to process
+            }
+        }
+        else {
+            var idx = this._rawData.indexOf(VSCodeProtocol.ProtocolServer.TWO_CRLF);
+            if (idx !== -1) {
+                var header = this._rawData.toString('utf8', 0, idx);
+                var lines = header.split('\r\n');
+                for (var i = 0; i < lines.length; i++) {
+                    var pair = lines[i].split(/: +/);
+                    if (pair[0] == 'Content-Length') {
+                        this._contentLength = +pair[1];
+                    }
+                }
+                this._rawData = this._rawData.slice(idx + VSCodeProtocol.ProtocolServer.TWO_CRLF.length);
+                continue;
+            }
+        }
+        break;
+    }
+};
+
+module.exports.ProtocolServer = ProtocolServer;

Martin1994/orion.collab

modules/orionode.debug.server/lib/debugServer.js
@@ -1,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+'use strict';
+
+var logger = require('./logger');
+var AdapterManager = require('./adapterManager');
+
+var CONNECTION_TIMEOUT = 10000;
+
+/**
+ * @param {Map.<string, DebugAdapter>} adapterPool
+ * @return {function}
+ */
+module.exports = function createDebugServer(adapterPool) {
+    return function(socket) {
+        var connectionId;
+        do {
+            connectionId = Math.floor(Math.random() * 0x1000000).toString(16);
+        } while (adapterPool.has(connectionId));
+        var adapter = null;
+
+        socket.emit('idle');
+
+        logger.log('Socket ' + connectionId + ' connected.', logger.MessageType.VERBOSE);
+
+        socket.on('disconnect', function() {
+            logger.log('Socket ' + connectionId + ' disconnected.', logger.MessageType.VERBOSE);
+            if (adapter) {
+                adapter.dispose();
+            }
+        });
+
+        socket.on('init', function(request, callback) {
+            try {
+                // Initialize new adapter
+                adapter = AdapterManager.createAdapter(request.arguments.adapterID);
+                adapterPool.set(connectionId, adapter);
+                adapter.on('request', function(request) {
+                    socket.emit('request', request, function(response) {
+                        adapter.sendResponse(response);
+                    });
+                });
+
+                adapter.on('event', function(event) {
+                    socket.emit('event', event);
+                });
+
+                adapter.on('disposed', function() {
+                    adapter = null;
+                    adapterPool.delete(connectionId);
+                    socket.emit('idle');
+                    logger.log(request.arguments.adapterID + ' adapter ended for ' + connectionId + '.', logger.MessageType.VERBOSE);
+                });
+
+                // Send launch command
+                adapter.sendRequest(request.command, request.arguments, CONNECTION_TIMEOUT, function(response) {
+                    if (response.success) {
+                        socket.emit('ready', connectionId);
+                    }
+                    if (callback) {
+                        callback(response);
+                    }
+                });
+
+                logger.log(request.arguments.adapterID + ' adapter started for ' + connectionId + '.', logger.MessageType.VERBOSE);
+            } catch (ex) {
+                socket.emit('fail', ex.message);
+                adapter = null;
+                adapterPool.delete(connectionId);
+            }
+        });
+
+        socket.on('request', function(request, callback) {
+            if (!adapter) {
+                socket.emit('fail', 'Not ready yet.');
+                return;
+            }
+            try {
+                adapter.sendRequest(request.command, request.arguments, CONNECTION_TIMEOUT, function(response) {
+                    if (callback) {
+                        callback(response);
+                    }
+                });
+            } catch (ex) {
+                socket.emit('fail', ex.message);
+            }
+        });
+
+        socket.on('event', function(event) {
+            adapter.sendEvent(event);
+        });
+    };
+};

modules/orionode.debug.server/lib/protocol.js
@@ -1,0 +1,82 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *--------------------------------------------------------------------------------------------*/
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *	 IBM Corporation - initial API and implementation
+ *******************************************************************************/
+'use strict';
+
+var VSCodeProtocol = require('vscode-debugadapter/lib/protocol');
+var Messages = require('vscode-debugadapter/lib/messages');
+
+var ProtocolServer = function() {
+    VSCodeProtocol.ProtocolServer.apply(this, arguments);
+};
+
+ProtocolServer.prototype = Object.create(VSCodeProtocol.ProtocolServer.prototype);
+
+ProtocolServer.prototype.handleEvent = function(event) {
+};
+
+// Add event handler
+ProtocolServer.prototype._handleData = function(data) {
+    this._rawData = Buffer.concat([this._rawData, data]);
+    while (true) {
+        if (this._contentLength >= 0) {
+            if (this._rawData.length >= this._contentLength) {
+                var message = this._rawData.toString('utf8', 0, this._contentLength);
+                this._rawData = this._rawData.slice(this._contentLength);
+                this._contentLength = -1;
+                if (message.length > 0) {
+                    try {
+                        var msg = JSON.parse(message);
+                        if (msg.type === 'request') {
+                            this.dispatchRequest(msg);
+                        }
+                        else if (msg.type === 'response') {
+                            var response = msg;
+                            var clb = this._pendingRequests.get(response.request_seq);
+                            if (clb) {
+                                this._pendingRequests.delete(response.request_seq);
+                                clb(response);
+                            }
+                        }
+                        else if (msg.type === 'event') {
+                            this.handleEvent(msg);
+                        }
+                    }
+                    catch (e) {
+                        this._emitEvent(new Messages.Event('error'));
+                    }
+                }
+                continue; // there may be more complete messages to process
+            }
+        }
+        else {
+            var idx = this._rawData.indexOf(VSCodeProtocol.ProtocolServer.TWO_CRLF);
+            if (idx !== -1) {
+                var header = this._rawData.toString('utf8', 0, idx);
+                var lines = header.split('\r\n');
+                for (var i = 0; i < lines.length; i++) {
+                    var pair = lines[i].split(/: +/);
+                    if (pair[0] == 'Content-Length') {
+                        this._contentLength = +pair[1];
+                    }
+                }
+                this._rawData = this._rawData.slice(idx + VSCodeProtocol.ProtocolServer.TWO_CRLF.length);
+                continue;
+            }
+        }
+        break;
+    }
+};
+
+module.exports.ProtocolServer = ProtocolServer;